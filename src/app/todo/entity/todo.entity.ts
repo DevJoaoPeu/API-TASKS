ee73e6b413fda9239e84d20432c26618b6fa8c49
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-26 09:55:18 -0300

    feat: Criando entidade do todo

---
+++ src/app/todo/entity/todo.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'todos' })
+export class TodoEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  task: string;
+  @Column({ name: 'is_done', type: 'tinyint', width: 1 })
+  isDone: string;
+  @CreateDateColumn({ name: 'created_at' })
+  createAt: string;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deleteAt: string;
+}
