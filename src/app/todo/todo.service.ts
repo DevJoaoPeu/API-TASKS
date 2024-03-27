f1a273a0e92bfafc61d930fb749f37106b61f2a2
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-27 08:39:00 -0300

    feat: Criando crud e fazendo conexão com banco

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { TodoEntity } from './entity/todo.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  async findAll() {
+    return await this.todoRepository.find();
+  }
+
+  async findOne(id: string) {
+    try {
+      return await this.todoRepository.findOneOrFail({ where: { id } });
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  async create(data) {
+    const responseCreated = await this.todoRepository.create(data);
+    return await this.todoRepository.save(responseCreated);
+  }
+
+  async update(id: string, data) {
+    const todo = await this.findOne(id)
+
+    this.todoRepository.merge(todo, data)
+    return await this.todoRepository.save(todo)
+  }
+
+  async deleteById(id: string) {
+    await this.findOne(id)
+    await this.todoRepository.softDelete(id)
+  }
+}
